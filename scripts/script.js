39d2a3108e75ebded97a7f7fc75bdb51e0bb4383
cedreaper <88981393+cedreaper@users.noreply.github.com>
2023-02-05 19:56:46 -0600

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -32,6 +32,8 @@
 let allSections = [];
 allSections = document.querySelectorAll("section");
 
+allSections[0].scrollIntoView({ behavior: "smooth" });
+
 //now If I am rendering my navigation-bar dynamically, I will then need reference to
 //the navBar (ul) list node. variable is plagiarised?? I'll change it
 //I guess.. what else do u want to call a navBar..? yet udacity calls it a
@@ -55,6 +57,8 @@ const sectionCount = allSections.length;
 //This function creates the entire navigation bar based on
 //the sections by creating a new <li> node for each section...
 function createDynamicNavigationBar() {
+  let idCount = 1;
+
   for (section of allSections) {
     //iterate over all sections
 
@@ -64,7 +68,9 @@ function createDynamicNavigationBar() {
     //are given the menu link and also the data-nav properties in the starter
     //code.. so I am making use of the given code here..
     navBarComponent.innerHTML =
-      "<a class='menu__link' href='#" +
+      "<a id='" +
+      idCount +
+      "' class='menu__link' href='#" +
       section.getAttribute("id") +
       "'> " +
       section.getAttribute("data-nav") +
@@ -74,7 +80,9 @@ function createDynamicNavigationBar() {
     //many ways, however, this project requires us to use 'appendChild' so
     //that is what is done here.. not plagiarized.. simply the last logical step
     //of my build function**
-    finalizeDynamicNavBar(navBarComponent);
+    finalizeDynamicNavBar(navBarComponent, section);
+
+    idCount++;
   }
 }
 
@@ -134,13 +142,45 @@ activeCheck = () => {
     //which is also common knowledge**
     //https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
 
-    isWithinViewport(section)
-      ? !section.classList.contains("active")
-        ? section.classList.add("active")
-        : 0
-      : section.classList.remove("active");
-  }
+    isWithinViewport(section) ? !section.classList.contains("active") ? doubleActive(section): 0 : doubleInactive(section);
+
 };
+
+//activate both section and nav at same time
+function doubleActive(section) {
+  let numberExtraction = section.getAttribute("id");
+
+  //get last char for ID #
+  let navId = numberExtraction.charAt(numberExtraction.length - 1);
+
+  //make navBar component active
+  let navComponent = document.getElementById(navId);
+
+  navComponent.classList.add("active-nav");
+
+  //make section active also
+  section.classList.add("active");
+
+  navComponent.style.color = "#000000";
+}
+
+//deactivate both section and nav
+function doubleInactive(section) {
+  //not following SOLID principles I know, just getting it done
+
+  let numberExtraction = section.getAttribute("id");
+
+  //get last char for ID #
+  let navId = numberExtraction.charAt(numberExtraction.length - 1);
+
+  //make navBar component inactive
+  let navComponent = document.getElementById(navId);
+
+  navComponent.classList.remove("active-nav");
+
+  //make section inactive also
+  section.classList.remove("active");
+}
 
 //build navbar dynamically
 //see function definition why would I put all the steps to build this here, when I can extract
@@ -150,6 +190,9 @@ createDynamicNavigationBar();
 //add one more thing to meet requirement for 'append'...
 //add on basic text after the navbar links
 navigationBar.append("<--Click For Section #");
+
+//create a global variable to all links after dynamic creation
+let allLinks = Array.from(document.querySelectorAll("a"));
 
 /**
  * End Main Functions
@@ -168,5 +211,24 @@ navigationBar.append("<--Click For Section #");
 //The skeleton code given to us with the
 //comments put in place has the events at the end of the file.. which is why this is here.
 //which im sure is similar to many students.. we are using what is given to us as starter code.
+for (link of allLinks) {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
 
+    const clickedNavItem = e.target;
+
+    //if section id === clicked id we found a match and are ready
+    //to scroll that section into view....
+    for (sect of allSections) {
+      if (
+        sect.getAttribute("id") ===
+        `section${clickedNavItem.getAttribute("id")}`
+      ) {
+        sect.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  });
+}
+
+//scroll event
 document.addEventListener("scroll", activeCheck);
